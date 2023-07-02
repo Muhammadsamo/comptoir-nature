866587218bfa86a0083fa1037bb8f96dd91457fd
kashif102 <kashifalitalpur@gmail.com>
2023-07-02 17:30:44 +0500

    created header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,3 +1,15 @@
 <template>
-  <header>Header</header>
+  <header class="bg-[#2D2D2D] px-[50px] py-4">
+    <div class="flex justify-between">
+      <nuxt-link to="/"><nuxt-img src="/header/logo.svg"></nuxt-img></nuxt-link>
+      <ul class="text-[#E9E3DE] flex justify-between items-center w-[25%]">
+        <li>Accueil</li>
+        <li>
+          <nuxt-link to="/ProductsAndServices">Produits and Services</nuxt-link>
+        </li>
+        <li><nuxt-link to="/Contact">Contact</nuxt-link></li>
+        <li><NuxtImg src="/header/instaLogo.svg"></NuxtImg></li>
+      </ul>
+    </div>
+  </header>
 </template>
