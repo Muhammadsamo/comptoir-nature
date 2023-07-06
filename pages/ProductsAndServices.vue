5e16c82480556dc884d1d763f0fadf1210c9bc9d
kashif102 <kashifalitalpur@gmail.com>
2023-07-07 04:11:02 +0500

    CreatedProductAndServices mobile design and TermOfUse modal

--- pages/ProductsAndServices.vue
+++ pages/ProductsAndServices.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="h-[calc(100vh-160px)]">
+  <main class="min-h-[calc(100vh-160px)] bg-[#2D2D2D]">
     <div
-      class="grid grid-cols-3 items-center gap-[70px] justify-between bg-[#2D2D2D] h-[100%] px-[50px]"
+      class="hidden lg:grid grid-cols-3 items-center gap-[70px] justify-between h-[100%] px-[50px]"
     >
       <div class="col-start-1 col-end-2 justify-self-end w-[300px]">
         <h1
@@ -82,6 +82,36 @@
           </li>
         </ul>
       </div>
+    </div>
+    <div class="px-[11px] lg:hidden">
+      <h1
+        class="text-[#E9E3DE] pt-16 font-Merriweather text-center text-[24px] leading-[45px] tracking-[2.4px]"
+      >
+        Nos Coordonnées
+      </h1>
+      <p
+        class="text-[#B2896B] mt-1 font-Montserrat border-t border-[#B2896B] text-[16px] py-2 leading-7 text-center"
+      >
+        00 Rue Ferdinand Forrest <br />
+        (en face de La Palette) <br />97122 Baie-Mahault (Jarry)
+      </p>
+      <h2
+        class="text-[#7B9698] border-y text-center text-[16px] leading-9 tracking-[1.6px] font-Montserrat font-bold border-[#B2896B]"
+      >
+        Tél. 05 90 38 93 05
+      </h2>
+      <h3
+        class="text-[#E9E3DE] mt-20 font-Merriweather text-center text-[24px] leading-[45px] tracking-[2.4px]"
+      >
+        Nos Horaires
+      </h3>
+      <p
+        class="text-[#B2896B] mt-1 font-Montserrat py-2 border-y border-[#B2896B] text-[16px] leading-7 text-center"
+      >
+        Lundi au vendredi: 9h à 18h <br />
+        Samedi: 9h à 13h <br />
+        Dimanche: Fermé
+      </p>
     </div>
   </main>
 </template>
